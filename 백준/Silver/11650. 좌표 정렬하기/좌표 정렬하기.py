87d3b6116e1a52ab3fa923b80d0ac99ddb45dfc6
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-08 16:48:16 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 340 ms, Memory: 52820 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,15 @@
+'''Solved date
+- 230608
+'''
+import sys
+
+input = sys.stdin.readline
+N = int(input().rstrip())
+coordinates = list()
+for _ in range(N):
+    x, y = map(int, input().split())
+    coordinates.append((x, y))
+coordinates_sorted = sorted(coordinates, key = lambda x : (x[0], x[1]))
+
+for idx in range(N):
+    print(coordinates_sorted[idx][0], coordinates_sorted[idx][1])
