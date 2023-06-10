3ed8038cfc71c3590165309a1d4a26a80006512e
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-10 14:35:22 +0900

    Update 좌표 압축.py

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -5,14 +5,14 @@ import sys
 
 
 input = sys.stdin.readline
-N = int(input().rstrip())
+_N = int(input().rstrip())
 numbers = list(map(int, input().rstrip().split()))
 
 sorted_numbers = sorted(numbers)
 numbers_idx = dict()
 idx = 0
 for num in sorted_numbers:
-    if num not in numbers_idx.keys():
+    if num not in numbers_idx:
         numbers_idx[num] = idx
         idx += 1
     else:
@@ -20,3 +20,15 @@ for num in sorted_numbers:
 
 for num in numbers:
     print(numbers_idx[num], end=' ')
+
+
+# Bruce
+import sys
+
+
+input = sys.stdin.readline
+N = int(input().rstrip())
+X_list = list(map(int, input().rstrip().split()))
+sorted_x_list = sorted(set(X_list))
+val2idx = {val: idx for idx, val in enumerate(sorted_x_list)}
+print(*map(val2idx.get, X_list))
