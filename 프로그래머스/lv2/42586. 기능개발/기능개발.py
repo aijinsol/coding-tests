72ad0401e78e7a6d56d0a183441e4736db28dd1d
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-11 13:50:57 +0900

    Update 기능개발.py

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -4,31 +4,28 @@
 from collections import deque
 
 
-work_days = deque()
 def solution(progresses, speeds):
+    # 1) Determine the number of days required for each task.
+    work_days = deque()
     for idx, progress in enumerate(progresses):
         n = 1
         while progress + speeds[idx] * n < 100:
             n += 1
         work_days.append(n)
-    print(work_days)
     
+    # 2) Identify the number of tasks being deployed per date.
     idx = 1
     ans = []
     len_check = len(work_days)
     while work_days:
         pivot = work_days[0]
-        print(f'{pivot=}')
         if len(work_days) == 1:
-            ans.append(idx)
+            ans.append(1)
             break
         if pivot < work_days[idx]:
             ans.append(idx)
-            print(f'{ans=}')
             for _ in range(idx):
                 work_days.popleft()
-            print(f'{work_days=}')
-            print('==='*5)
             idx = 1
         else:
             idx += 1
@@ -36,4 +33,25 @@ def solution(progresses, speeds):
                 ans.append(idx)
                 break
     
+    return ans
+
+# Bruce
+from collections import deque
+
+
+def solution(progresses, speeds):
+    ans = []
+    queue = deque([[progress, speed] for progress, speed in zip(progresses, speeds)])
+    while queue:
+
+        cnt = 0
+        for idx, (progress, speed) in enumerate(queue):
+            queue[idx][0] += speed
+
+        while queue and queue[0][0] >= 100:
+            queue.popleft()
+            cnt += 1
+
+        if cnt:
+            ans.append(cnt)
     return ans
