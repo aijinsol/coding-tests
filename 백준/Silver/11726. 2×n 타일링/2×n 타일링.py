96698ee00e11dc966f77c71b43782c1d80e763b4
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-11 15:22:14 +0900

    Update 2×n 타일링.py

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -4,16 +4,17 @@ import sys
 def tile_cnt(num):
     dp = [1, 1]
     for i in range(2, num+1):
-        dp.append(d[i-1] + d[i-2])
+        val = (d[i-1] + d[i-2]) % 10007
+        dp.append(val)
     return dp[num]
 
 def tile_cnt(num):
     dp = [0] * (num+1)
     dp[0], dp[1] = 1, 1
     for i in range(2, num+1):
-        dp[i] = dp[i-1] + dp[i-2]
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
     return dp[num]
 
 input = sys.stdin.readline
 n = int(input().strip())
-print(tile_cnt(n) % 10007)
+print(tile_cnt(n))
