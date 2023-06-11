94cca5c9528b1b0b70753f1a8e687fd715840b25
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-11 14:56:49 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def tile_cnt(num):
+    dp = [1, 1]
+    for i in range(2, num+1):
+        dp.append(d[i-1] + d[i-2])
+    return dp[num]
+
+def tile_cnt(num):
+    dp = [0] * (num+1)
+    dp[0], dp[1] = 1, 1
+    for i in range(2, num+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[num]
+
+n = int(input())
+print(tile_cnt(n) % 10007)
