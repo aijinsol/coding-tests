89dba4dcf9a052092ea3407f5325d10223343b78
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-14 14:14:55 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,16 @@
+'''Solved date
+- 230614
+'''
+import sys
+
+
+def solution(N, scores):
+    max_score = max(scores)
+    new_scores = map(lambda x: x/max_score*100, scores)
+    ans = float(sum(new_scores) / N)
+    return ans
+
+input = sys.stdin.readline
+N = int(input().rstrip())
+scores = list(map(int, input().split()))
+print(solution(N, scores))
