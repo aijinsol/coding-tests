41890664391a6b6d231b6ec718088c25bc8d052f
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-14 14:17:44 +0900

    Update 평균.py

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -6,7 +6,7 @@ import sys
 
 def solution(N, scores):
     max_score = max(scores)
-    new_scores = map(lambda x: x/max_score*100, scores)
+    new_scores = map(lambda x: x / max_score * 100, scores)
     ans = float(sum(new_scores) / N)
     return ans
 
