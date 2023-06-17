4a9076103dc9e5a483e7d488e263dba1606eabd1
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-17 15:18:41 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,6 @@
 '''Solved date
-- 230614
+- 230617
 '''
-# def solution(participant, completion):
-#     for p in participant:
-#         if p in completion:
-#             completion.remove(p)
-#         else:
-#             return p
-
 def solution(participant, completion):
     p_acc, c_acc = 0, 0
     hashmap = dict()
@@ -20,4 +13,28 @@ def solution(participant, completion):
         c_acc += c_hash
         
     ans = hashmap[p_acc - c_acc]
+    return ans
+
+
+def solution(participant, completion):
+    acc = 0
+    hashmap = dict()
+    for p in participant:
+        hash_val = hash(p)
+        hashmap[hash_val] = p
+        acc += hash_val
+    for c in completion:
+        acc -= hash(c)
+        
+    ans = hashmap[acc]
+    return ans
+
+
+from collections import Counter
+
+
+def solution(participant, completion):
+    p = Counter(participant)
+    c = Counter(completion)
+    ans = list(p - c)[0]
     return ans
