7672553b560096b43bdf9016995b89ff950089ee
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-24 13:53:49 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.86 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -15,3 +15,19 @@ def solution(numbers):
     
     ans = sorted(list(ans))
     return ans
+
+
+def solution(numbers):
+    ans = set()
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            ans.add(numbers[i] + numbers[j])
+    return sorted(list(ans))
+
+
+from itertools import combinations
+
+
+def solution(numbers):
+    ans = sorted(set(sum(i) for i in list(combinations(numbers, 2))))
+    return ans
