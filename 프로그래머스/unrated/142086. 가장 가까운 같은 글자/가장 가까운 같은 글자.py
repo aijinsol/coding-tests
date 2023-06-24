2b081a43f74526501b4e7111cfaa2a0c2f9e97e4
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-24 17:03:25 +0900

    Update 가장 가까운 같은 글자.py

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -10,3 +10,15 @@ def solution(s):
             idx = abs(s[:i][::-1].find(c)) + 1
             ans.append(idx)
     return ans
+
+# Bruce
+def solution(s):
+    ans = []
+    hashmap = dict()
+    for idx, c in enumerate(s):
+        if c not in hashmap:
+            ans.append(-1)
+        else:
+            ans.append(idx - hashmap[c])
+        hashmap[c] = idx
+    return ans
