641c874500f87752462726a4ccf17a1930227856
Jinsol Kim <jinsolkim719@gmail.com>
2023-06-30 17:03:03 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -15,3 +15,15 @@ def solution(name, yearning, photo):
                 continue
         ans.append(score)
     return ans
+
+
+def solution(name, yearning, photo):
+    hash_map = dict(zip(name, yearning))
+    ans = []
+    for p in photo:
+        score = 0
+        for p_name in p:
+            if p_name in hash_map:
+                score += hash_map[p_name]
+        ans.append(score)
+    return ans
