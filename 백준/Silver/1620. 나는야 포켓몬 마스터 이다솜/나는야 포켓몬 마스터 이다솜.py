c4a6098052a908fc209186ec810eabb781df79cb
Jinsol Kim <jinsolkim719@gmail.com>
2023-07-03 22:16:47 +0900

    Update 나는야 포켓몬 마스터 이다솜.py

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -21,3 +21,22 @@ for _ in range(M):
         print(hashmap[int(question)])
     else:
         print(hashmap_converted[question])
+
+
+# Bruce
+import sys
+
+
+input = sys.stdin.readline
+N, M = map(int, input().rstrip().split())
+hashmap = dict()
+for n in range(1, N+1):
+    pokemon = input().rstrip()
+    hashmap[n] = pokemon
+    hashmap[pokemon] = n
+    
+for m in range(M):
+    question = input().rstrip()
+    if question.isdigit():
+        question = int(question)
+    print(hashmap[question])
