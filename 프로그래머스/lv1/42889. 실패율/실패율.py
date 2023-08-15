2df197a18491d38f5641da3d0fda8886f1beefb2
Jinsol Kim <jinsolkim719@gmail.com>
2023-08-15 16:43:58 +0900

    [level 1] Title: 실패율, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,28 @@
+'''Solved date
+- 230815
+'''
+from collections import defaultdict
+
+
+def solution(N, stages):
+    ans = []
+    user_cur_stage = defaultdict(int)
+    fail_rates = defaultdict(int)
+    for stage in range(1, N+1+1):
+        user_cur_stage[stage] = stages.count(stage)
+    
+    for stage in range(1, N+1):
+        challengers = 0
+        for key, val in user_cur_stage.items():
+            if key >= stage:
+                challengers += val
+        if challengers != 0:
+            fail_rates[stage] = user_cur_stage[stage] / challengers
+        else:
+            fail_rates[stage] = 0
+    
+    fail_rates_sorted = sorted(fail_rates.items(), reverse=True, key=lambda x: x[1])
+    for rates in fail_rates_sorted:
+        ans.append(rates[0])
+
+    return ans
