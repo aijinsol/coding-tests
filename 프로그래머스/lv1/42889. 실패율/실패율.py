87ea4338446500c37957bd3155d7483346024c97
Jinsol Kim <jinsolkim719@gmail.com>
2023-08-15 16:55:59 +0900

    Update 실패율.py

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -19,10 +19,25 @@ def solution(N, stages):
         if challengers != 0:
             fail_rates[stage] = user_cur_stage[stage] / challengers
         else:
-            fail_rates[stage] = 0
+            fail_rates[stage]
     
     fail_rates_sorted = sorted(fail_rates.items(), reverse=True, key=lambda x: x[1])
     for rates in fail_rates_sorted:
         ans.append(rates[0])
 
+    return ans
+
+
+def solution(N, stages):
+    ans = []
+    fail_rates = defaultdict(int)
+    for stage in range(1, N+1):
+        if stages:
+            fail_rates[stage] = stages.count(stage) / len(stages)
+            stages = [n for n in stages if n != stage]
+        else:
+            fail_rates[stage]
+    fail_rates_sorted = sorted(fail_rates.items(), reverse=True, key=lambda x: x[1])
+    for rates in fail_rates_sorted:
+        ans.append(rates[0])
     return ans
