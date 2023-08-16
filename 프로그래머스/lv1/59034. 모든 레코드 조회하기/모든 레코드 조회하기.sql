533ed1f563402d556b7de7fd7cb7c7ab886d39e6
Jinsol Kim <jinsolkim719@gmail.com>
2023-08-16 13:52:43 +0900

    [level 1] Title: 모든 레코드 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
@@ -0,0 +1,14 @@
+/* Solved date
+- 230816
+*/
+SELECT
+    ANIMAL_ID
+    , ANIMAL_TYPE
+    , DATETIME
+    , INTAKE_CONDITION
+    , NAME
+    , SEX_UPON_INTAKE
+FROM
+    ANIMAL_INS
+ORDER BY
+    ANIMAL_ID;
