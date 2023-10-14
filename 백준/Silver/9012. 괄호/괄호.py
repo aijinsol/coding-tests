8f3804c8b5493b9c422bb3728cdcc3bc2c869b32
Jinsol Kim <jinsolkim719@gmail.com>
2023-10-14 20:12:38 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -3,27 +3,46 @@
 '''
 import sys
 
-
 input = sys.stdin.readline
 T = int(input().rstrip())
 
-def is_vps(ps: str) -> str:
+
+def is_vps(ps: str) -> None:
     stack = []
     for p in ps:
-        if not stack: # 비어있을 때
-            stack.append(p)
-        else: # 무언가 있을 때
-            if stack[-1] == '(':
-                if p == ')':
-                    stack.pop()
-                else:  # p == '('
-                    stack.append(p) 
-            else:  # stack[-1] == ')'
+        if stack: # 무언가 있을 때
+            if stack[-1] == '(' and p == ')':
+                stack.pop()
+            else:
                 stack.append(p)
-    if stack:
-        return 'NO'
-    return 'YES'
+        else: # 비어있을 때
+            stack.append(p)
+            
+    print('NO') if stack else print('YES')
 
 for _ in range(T):
     ps = input().rstrip()
-    print(is_vps(ps))
+    is_vps(ps)
+    
+    
+# Bruce
+'''
+import sys
+
+
+def chk_vps(ps: str):
+    stack = []
+    for c in ps:
+        if stack and stack[-1] == "(" and c == ")":
+            stack.pop()
+        else:
+            stack.append(c)
+    print("YES") if not stack else print("NO")
+
+
+input = sys.stdin.readline
+T = int(input().rstrip())
+for _ in range(T):
+    ps = input().rstrip()
+    chk_vps(ps)
+'''
