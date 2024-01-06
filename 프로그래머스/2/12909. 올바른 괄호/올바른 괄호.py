c79474c8a3146e09d80394742a9d9042d7cf0d3a
Jinsol Kim <jinsolkim719@gmail.com>
2024-01-06 18:48:28 +0900

    [level 2] Title: 올바른 괄호, Time: 10.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+'''Date
+- 240106
+'''
+def solution(s):
+    stack = []
+    for p in s:
+        if stack and stack[-1] == '(' and p == ')':
+            stack.pop()
+        else:
+            stack.append(p)
+    ans = False if stack else True
+    return ans
