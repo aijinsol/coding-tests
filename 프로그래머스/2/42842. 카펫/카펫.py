daf772dcac95c8ab3e79a9223f5163c64c6c9af0
Jinsol Kim <jinsolkim719@gmail.com>
2024-01-23 19:14:34 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -4,12 +4,28 @@
 def solution(brown, yellow):
     num_blocks = brown + yellow
     candidates = []
-    for num in range(3, num_blocks//2):
+    for num in range(3, num_blocks):
         quotient, remainder = divmod(num_blocks, num)
+        if num > quotient:
+            break
         if remainder == 0:
             candidates.append([quotient, num])
-    
+            
     for candidate in candidates:
         row, col = candidate
         if (row-2) * (col-2) == yellow:
             return [row, col]
+        
+
+def solution(brown, red):
+    for i in range(1, int(red**(1/2))+1):
+        if red % i == 0:
+            if 2*(i + red//i) == brown-4:
+                return [red//i+2, i+2]
+            
+            
+def solution(brown, yellow):
+    area = brown + yellow
+    for num in range(3, int(area ** 1/2) + 1):
+        if (area % num == 0) and 2 * (area // num + num - 2) == brown:
+            return [area // num, num]
