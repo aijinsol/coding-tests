0870257ef7dff6da84ed2cf9c1753f003bc52e0c
Jinsol Kim <jinsolkim719@gmail.com>
2024-02-05 16:26:39 +0900

    [Silver III] Title: N과 M (3), Time: 2076 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,16 @@
+'''Date
+- 240205
+'''
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().rstrip().split())
+
+def recur(N, M, sequence=[]):
+    if len(sequence) == M:
+        print(*sequence)
+        return
+    for num in range(1, N+1):
+        recur(N, M, sequence+[num])
+
+recur(N, M)
