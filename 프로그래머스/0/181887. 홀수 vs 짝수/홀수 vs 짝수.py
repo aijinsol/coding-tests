c3646bd37d6d2de422ccc9b82deea8de6d40afc6
Jinsol Kim <jinsolkim719@gmail.com>
2024-10-07 17:46:42 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,11 +1,4 @@
 def solution(num_list):
-    sum_odd, sum_even = 0, 0
-    for idx, num in enumerate(num_list, start=1):
-        if idx % 2 != 0:
-            sum_odd += num
-        else:
-            sum_even += num
-    
-    ans = max(sum_odd, sum_even) if sum_odd != sum_even else sum_odd
-    
-    return ans
+    sum_odd = sum(num_list[::2])
+    sum_even = sum(num_list[1::2])
+    return max(sum_odd, sum_even)
