bc99843cda89b99923333738814caa337357f2e9
Jinsol Kim <jinsolkim719@gmail.com>
2024-10-09 19:15:08 +0900

    [level 0] Title: 옹알이 (1), Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,7 +1,15 @@
+from itertools import permutations
+
+
 def solution(babbling):
+    words = ["aya", "ye", "woo", "ma"]
+    candidates = set()
+    for i in range(1, len(words) + 1):
+        for p in permutations(words, i):
+            candidates.add("".join(p))
+            
     ans = 0
-    for babble in babbling:
-        for pivot in ["aya", "ye", "woo", "ma"]:
-            babble = babble.replace(pivot, ' ')
-        ans += 1 if not babble.strip() else 0
+    for b in babbling:
+        if b in candidates:
+            ans +=1
     return ans
