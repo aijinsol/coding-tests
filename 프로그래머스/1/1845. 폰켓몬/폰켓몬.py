f7f66d00fba45b3639032ae88b44beb5dd1f884a
Jinsol Kim <jinsolkim719@gmail.com>
2024-12-16 19:09:51 +0900

    [level 1] Title: 폰켓몬, Time: 0.82 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,5 @@
 def solution(nums):
-    unique_nums = set(nums)
-    if len(unique_nums) <= len(nums)//2:
-        ans = len(unique_nums)
-    else:
-        ans = len(nums)//2
+    ans = min(len(nums)//2, len(set(nums)))
     return ans
 
 # 전체 숫자 갯수 n
@@ -13,5 +9,7 @@ def solution(nums):
 # [1,1,1,1,1,1,1,1,1,1] -> 1
 # [1,2,1,1,1,1,1,1,1,1] -> 2
 # [1,2,2,1,1,1,1,1,1,1] -> 2
+# [1,2,2,3,1,1,1,1,1,1] -> 3
+# [1,2,2,3,4,1,1,1,1,1] -> 4
 # [1,2,3,4,5,5,1,1,1,1] -> 5
 # [1,2,3,4,5,6,1,1,1,1] -> 5
